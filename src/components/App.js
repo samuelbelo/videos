276f2ce7f07ed30cbac0a5e142d821c916b5640d
Samuel Belo <sbelo@vertigo.com.br>
2019-06-19 16:08:54 -0300

    passing state as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 import youtube from '../apis/youtube'
+import VideoList from './VideoList'
 
 class App extends React.Component {
     state = { videos: [] }
@@ -18,7 +19,7 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
-                I've found {this.state.videos.length} videos there :P
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
